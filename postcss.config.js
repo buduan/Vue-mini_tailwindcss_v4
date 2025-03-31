cc29d286d3e514fd2707e3ca73c8bb0cfd0fafe2
不断同学 <buduan461@qq.com>
2025-03-31 12:43:55 +0800

    Init

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+import pxtorpx from 'postcss-pxtorpx-pro';
+import tailwindcss from '@tailwindcss/postcss';
+
+const config = {
+  plugins: [
+    {
+      postcssPlugin: 'postcss-import-css-to-wxss',
+      AtRule: {
+        import: (atRule) => {
+          atRule.params = atRule.params.replace('.css', '.wxss');
+        },
+      },
+    },
+    pxtorpx({ transform: (x) => x }),
+  ],
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'weapp-vite/config'
+import tailwindcss from '@tailwindcss/vite'
+import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
+
+
+export default defineConfig({
+  weapp: {
+    srcRoot: 'src',
+
+  },
+  plugins: [
+    UnifiedViteWeappTailwindcssPlugin({
+      rem2rpx: true,
+      tailwindcss: {
+        // 显示声明用的是 tailwindcss v4
+        version: 4,
+        v4: {
+          cssEntries: [
+            // app.css 的路径
+            'src/app.css',
+          ],
+        },
+      },
+    }),
+  ],
+})
